e5e4ed47fc16b887c86b63ee9112bb21359b84df
Mary Lebedinova <anisimmar@gmail.com>
2021-08-30 19:20:03 +0300

    Пицца выделена в отдельный компонент

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import {Route} from "react-router-dom";
 
 import {Header} from './components/index'
 import {Home, Cart} from "./pages/index";
+import axios from "axios";
 
 function App() {
+const [pizzas, setPizzas] = React.useState([])
+
+    React.useEffect(() => {
+        axios.get('http://localhost:3000/db.json')
+            .then((response) => {
+                setPizzas(response.data.pizzas)
+            })
+    }, [])
+
+
     return (
         <div className="wrapper">
             <Header/>
             <div className="content">
-                <Route path='/' component={Home} exact/>
+                <Route path='/' render={() => <Home  items={pizzas}/>} exact/>
                 <Route path='/cart' component={Cart} exact/>
             </div>
         </div>
